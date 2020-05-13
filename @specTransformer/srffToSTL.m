90bea3dc26dd99bc7c9926c541fded89f0d6710e
jeddelan <johan.eddeland@volvocars.com>
2020-05-13 08:54:18 +0200

    Split srff into two cases

--- @specTransformer/srffToSTL.m
+++ @specTransformer/srffToSTL.m
@@ -6,75 +6,132 @@ inputNames = obj.getInputNames(component);
 [startDelay1, endDelay1, depth1, modalDepth1, FPIstruct1] = obj.getSubStructInfo(inputNames{1}); % s
 [startDelay2, endDelay2, depth2, modalDepth2, FPIstruct2] = obj.getSubStructInfo(inputNames{2}); % r
 
-% Now, find the delay block in the subsystem
-delayBlock = find_system([get(component,'Path') '/' get(component,'Name')],'LookUnderMasks','On','FollowLinks','On','BlockType','UnitDelay');
-delayBlock = delayBlock{1};
-delayBlockHandle = get_param(delayBlock, 'Handle');
+% The new FPI struct will have the same prerequisites as R (input 2), but
+% we will change the formula to also include "and once(S)"
+newFPIstruct = struct();
 
-% Log the delay block (it is used in the 
-logSigName = obj.logOneBlock(delayBlockHandle);
+% Keep track of if we have logged the delay block or not
+% (We need to log it if R ~= 0 for any entry in FPIstruct)
+% See details below
+delayBlockLogged = 0;
 
-% We use Q to write the formula in a more readable form
-Q = [logSigName '[t]'];
+% Keep track of if at least one R == 0
+atLeastOneRZero = 0;
 
-FPIstruct = struct();
-
-for tmpIndex=1:length(FPIstruct1)
-    term1 = FPIstruct1(tmpIndex).formula;
+% NEW, WITH WRAP-UP
+for tmpIndex2=1:length(FPIstruct2)
+    % There are two different cases for each entry in the FPIstruct
+    % 1. If R == 0, the formula is essentially once(S) over the whole
+    %    simulation
+    % 2. If R ~= 0, we cannot express the formula in STL. We would need
+    %    something like STL*, since the formula is something like
+    %    not(R[t]) and once_[0, t*](S[t])
+    %    where t* is the time since R was last true. 
     
-    for tmpIndex2=1:length(FPIstruct2)
-        term2 = FPIstruct2(tmpIndex2).formula;
+    % This means that if R ~= 0, we simply have to log the output of the
+    % delay block and write the formula with respect to that.
+    term2 = FPIstruct2(tmpIndex2).formula;
+    try
+        term2_evaluated = eval(term2);
+    catch
+        term2_evaluated = 1;
+    end
+    
+    if term2_evaluated == 0
+        % innerFormula is essentially "once_[0, endTime](S)", where S is the
+        % first input. However, if S has multiple FPI struct entries, we need
+        % to take care of that manually with the following for-loops.
+        innerFormula = ['once_[0, ' num2str(obj.endTime) ']('];
         
-        FPIstruct(end+1).prereqSignals = {FPIstruct1(tmpIndex).prereqSignals{:}, FPIstruct2(tmpIndex2).prereqSignals{:}}; %#ok<AGROW>
-        
-        % Update prereqFormula
-        if isempty(FPIstruct1(tmpIndex).prereqFormula)
-            if isempty(FPIstruct2(tmpIndex2).prereqFormula)
-                FPIstruct(end).prereqFormula = '';
-            else
-                FPIstruct(end).prereqFormula = FPIstruct2(tmpIndex2).prereqFormula;
-            end
-        else
-            if isempty(FPIstruct2(tmpIndex2).prereqFormula)
-                FPIstruct(end).prereqFormula = FPIstruct1(tmpIndex).prereqFormula;
-            else
-                FPIstruct(end).prereqFormula = [FPIstruct1(tmpIndex).prereqFormula ' and ' FPIstruct2(tmpIndex2).prereqFormula];
-            end
+        for tmpIndex1=1:length(FPIstruct1)-1
+            innerFormula = [innerFormula '(' FPIstruct1(tmpIndex1).prereqFormula ' and ' FPIstruct(tmpIndex1).formula ') or']; %#ok<*AGROW>
         end
         
-        try
-            term2_evaluated = eval(term2);
-        catch
-            term2_evaluated = 1;
+        if length(FPIstruct1) == 1
+            innerFormula = [innerFormula FPIstruct1(end).formula];
+        else
+            innerFormula = [innerFormula '(' FPIstruct1(end).prereqFormula ' and ' FPIstruct1(end).formula ')'];
+        end
+        innerFormula = [innerFormula ')'];
+        
+        % Add a new entry to newFPIstruct
+        newFPIstruct(end+1).prereqSignals = FPIstruct2(tmpIndex2).prereqSignals;
+        newFPIstruct(end).prereqFormula = FPIstruct2(tmpIndex2).prereqFormula;
+        newFPIstruct(end).formula = ...
+            ['(not(' FPIstruct2(tmpIndex2).formula ') and ' ...
+                innerFormula ')'];
+            
+        % At least one R is zero
+        atLeastOneRZero = 1;
+        
+    else
+        % R != 0
+        % Formula: (not(R[t]) and (S[t] or Q[t-1]))
+        
+        % Check if we need to log the delay block, or if it has already
+        % been done
+        if ~delayBlockLogged
+            delayBlock = find_system([get(component,'Path') '/' get(component,'Name')],'LookUnderMasks','On','FollowLinks','On','BlockType','UnitDelay');
+            delayBlock = delayBlock{1};
+            delayBlockHandle = get_param(delayBlock, 'Handle');
+            
+            % Log the delay block (it is used in the
+            logSigName = obj.logOneBlock(delayBlockHandle);
+            
+            % We use Q to write the formula in a more readable form
+            Q = [logSigName '[t]'];
+            
+            % Finally, now we note that we HAVE logged the delay block
+            delayBlockLogged = 1;
         end
         
-        if term2_evaluated == 0
-            % If R == 0, set the formula to the
-            % following: (S[t] or Q[t-1])
-            % Note that the signal we have logged as Q is indeed a
-            % time-delayed signal, so we need not shift it backwards in
-            % time
-            FPIstruct(end).formula = ['(' term1 ' or ' Q ')'];
-        else
-            % R != 0
-            % Formula: (not(R[t]) and (S[t] or Q[t-1]))
-            FPIstruct(end).formula = ['(not(' term2 ') and (' term1 ' or ' Q '))'];
+        % Create the actual formula
+        for tmpIndex1=1:length(FPIstruct1)
+            % Update prereqSignals
+            newFPIstruct(end+1).prereqSignals = {FPIstruct1(tmpIndex1).prereqSignals{:}, FPIstruct2(tmpIndex2).prereqSignals{:}};
+            
+            % Update prereqFormula
+            if isempty(FPIstruct1(tmpIndex1).prereqFormula)
+                if isempty(FPIstruct2(tmpIndex2).prereqFormula)
+                    newFPIstruct(end).prereqFormula = '';
+                else
+                    newFPIstruct(end).prereqFormula = FPIstruct2(tmpIndex2).prereqFormula;
+                end
+            else
+                if isempty(FPIstruct2(tmpIndex2).prereqFormula)
+                    newFPIstruct(end).prereqFormula = FPIstruct1(tmpIndex1).prereqFormula;
+                else
+                    newFPIstruct(end).prereqFormula = [FPIstruct1(tmpIndex1).prereqFormula ' and ' FPIstruct2(tmpIndex2).prereqFormula];
+                end
+            end
+            
+            % Update formula
+            newFPIstruct(end).formula = ['(not(' term2 ') and (' ...
+                FPIstruct1(tmpIndex1).formula ' or ' Q '))'];
         end
     end
 end
-FPIstruct(1) = [];
 
-thisStartDelay = max(startDelay1 + 1, startDelay2 + 1);
+% Remove the first empty entry in the newFPIstruct
+newFPIstruct(1) = [];
+
+thisStartDelay = max(startDelay1, startDelay2);
 thisEndDelay = max(endDelay1, endDelay2);
 thisDepth = max(depth1 + 2, depth2 + 2);
-thisModalDepth = max(modalDepth1 + 1, modalDepth2 + 1);
+if atLeastOneRZero
+    % At least one R is zero
+    % Modal depth is increased
+    thisModalDepth = max(modalDepth1 + 1, modalDepth2);
+else
+    thisModalDepth = max(modalDepth1, modalDepth2);
+end
 
 updateStruct = struct();
 updateStruct.startDelay = thisStartDelay;
 updateStruct.endDelay = thisEndDelay;
 updateStruct.depth = thisDepth;
 updateStruct.modalDepth = thisModalDepth;
-updateStruct.FPIstruct = FPIstruct;
+updateStruct.FPIstruct = newFPIstruct;
 updateStruct.type = 'phi_exp';
 updateStruct.component = component;
 
